3a39ce6674236645c55709242124223845d16adc
masatoshiwatanabe <masatoshi.watanabe@fox-hound.jp>
2025-09-26 20:12:00 +0900

    タスク3-3 途中

---
+++ app/users/[id]/details/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useParams } from "next/navigation";
+const UserDetailsPage:React.FC=()=>{
+    const {id}=useParams<{id:Number}>();
+    const 
+    return()
+}
+export default UserDetailsPage
