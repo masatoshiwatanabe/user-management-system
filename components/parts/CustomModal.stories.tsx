8f3e211623b3e5465a02b74996444cc630fe341e
masatoshiwatanabe <masatoshi.watanabe@fox-hound.jp>
2025-09-29 17:59:38 +0900

    タスク3-2　完了

--- components/parts/CustomModal.stories.tsx
+++ components/parts/CustomModal.stories.tsx
@@ -1,30 +1,39 @@
 // components/parts/CustomModal.stories.tsx
-
 import React, { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import CustomModal from "./CustomModal";
 import CustomButton from "./CustomButton";
 import { Box } from "@mui/material";
 
-// TODO: メタデータ
-// TODO: ストーリーの定義
+// メタデータ
+const meta: Meta<typeof CustomModal> = {
+  title: "Components/Parts/CustomModal",
+  component: CustomModal,
+};
+export default meta;
 
-// TODO: デフォルトストーリーの作成
+type Story = StoryObj<typeof CustomModal>;
+
+// デフォルトストーリー
 export const Default: Story = {
   render: () => {
     const [open, setOpen] = useState(false);
 
     return (
       <Box>
-        // TODO: クリックでモーダル開閉させる
-        <CustomButton variantType="primary" onClick={}>
+        {/* ボタンクリックでモーダルを開く */}
+        <CustomButton variantType="primary" onClick={() => setOpen(true)}>
           モーダルを開く
         </CustomButton>
         <CustomModal
-        // TODO: Propを渡す
-        // onCloceはsetOpenにfalseを渡す
-        // onConfirmはalert()を使ってクリックしたことを知らせて
-        // setOpenにfalseを渡す
+          open={open}
+          title="モーダルタイトル"
+          content="ここにモーダルの内容が入ります。"
+          onClose={() => setOpen(false)}
+          onConfirm={() => {
+            alert("確認ボタンがクリックされました！");
+            setOpen(false);
+          }}
         />
       </Box>
     );
