d7dd702f5e704347c01bf7f732a25cf090b076db
masatoshiwatanabe <masatoshi.watanabe@fox-hound.jp>
2025-09-29 12:35:52 +0900

    3-3 終了

--- app/users/[id]/details/page.tsx
+++ app/users/[id]/details/page.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-const UserDetailsPage:React.FC=()=>{
-    const {id}=useParams<{id:Number}>();
-    const 
-    return()
-}
-export default UserDetailsPage
+import { Typography, Alert, CircularProgress } from "@mui/material";
+import { fetchUserById } from "@/utils/api";
+import UserDetails from "@/components/UserDetails";
+import type { User } from "@/types/User";
+const UserDetailsPage: React.FC = () => {
+  const id = useParams().id;
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  if (!id || Array.isArray(id)) {
+    return <Typography>ユーザーIDが無効です</Typography>;
+  }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const data = await fetchUserById(Number(id));
+        setUser(data);
+      } catch (error) {
+        setError("ユーザーの取得に失敗しました。" + error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [id]);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  if (error) {
+    return <Alert severity="error">{error}</Alert>;
+  }
+  if (user == null) {
+    return <Typography>ユーザー情報が存在しません</Typography>;
+  }
+  return <UserDetails user={user} />;
+};
+export default UserDetailsPage;
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -9,16 +9,13 @@ import Link from "next/link";
 import React from "react";
 import { User } from "../types/User";
 import DeleteUserButton from "./DeleteUserButton";
-import { useState,} from "react";
 
 interface UserCardProps {
   user: User;
   onDelete: (userId: number) => void;
 }
 
-
-const UserCard: React.FC<UserCardProps> = ({ user,onDelete}) => {
-  
+const UserCard: React.FC<UserCardProps> = ({ user, onDelete }) => {
   return (
     <Card sx={{ minWidth: 275, mb: 2 }}>
       <CardContent>
@@ -29,10 +26,23 @@ const UserCard: React.FC<UserCardProps> = ({ user,onDelete}) => {
         <Typography variant="body2">役割: {user.role}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" component={Link} href={`/users/${user.id}/edit`}>
+        <Button
+          variant="outlined"
+          size="small"
+          component={Link}
+          href={`/users/${user.id}/details`}
+        >
+          詳細
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
+          component={Link}
+          href={`/users/${user.id}/edit`}
+        >
           編集
         </Button>
-        <DeleteUserButton userId={user.id} onDelete={onDelete}/>
+        <DeleteUserButton userId={user.id} onDelete={onDelete} />
       </CardActions>
     </Card>
   );
