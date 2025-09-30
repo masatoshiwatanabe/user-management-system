3f88ce244240384d966ebb2cb9851ccf6f131121
masatoshiwatanabe <masatoshi.watanabe@fox-hound.jp>
2025-09-30 15:12:21 +0900

    CustomButton 完了

--- components/parts/CustomButton.stories.tsx
+++ components/parts/CustomButton.stories.tsx
@@ -2,6 +2,9 @@
 
 import type { Meta, StoryObj } from "@storybook/react";
 import CustomButton from "./CustomButton";
+import { Save } from "@mui/icons-material";
+import { CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const meta: Meta<typeof CustomButton> = {
   title: "Components/Parts/CustomButton",
@@ -30,5 +33,50 @@ export const Danger: Story = {
   args: {
     variantType: "danger",
     children: "Danger Button",
+  },
+};
+
+export const Gradient: Story = {
+  args: {
+    variantType: "primary",
+    children: "Gradient Button",
+    style: {
+      background: "linear-gradient(45deg, #2196F3 30%, #9C27B0 90%)",
+      color: "#fff",
+    },
+  },
+};
+export const Icon: Story = {
+  args: {
+    variantType: "primary",
+    children: (
+      <>
+        <Save sx={{ mr: 1 }} /> 
+        Save
+      </>
+    ),
+  }
+}
+
+export const LoadingButton: Story = {
+  render: (args) => {
+    const [loading, setLoading] = useState(false);
+
+    const handleClick = () => {
+      setLoading(true);
+      setTimeout(() => setLoading(false), 3000); // 3秒後に再度押せる
+    };
+
+    return (
+      <CustomButton {...args} disabled={loading} onClick={handleClick}>
+        {loading && (
+          <CircularProgress
+            size={16}
+            style={{ marginRight: 8, color: "white" }}
+          />
+        )}
+        {loading ? "Loading..." : "Click Me"}
+      </CustomButton>
+    );
   },
 };
