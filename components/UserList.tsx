6e64d0b3bc42b17d3ee27cce65029a4d6939f0c6
masatoshiwatanabe <masatoshi.watanabe@fox-hound.jp>
2025-09-30 14:37:42 +0900

    タスク3-3 完了

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -4,6 +4,7 @@ import { softDeleteUser } from "@/utils/api";
 import CustomCard from "./parts/CustomCard";
 import { Button, Link } from "@mui/material";
 import CustomButton from "./parts/CustomButton";
+import CustomModal from "./parts/CustomModal";
 
 interface UserListProps {
   users: User[];
@@ -11,14 +12,31 @@ interface UserListProps {
 
 export const UserList: React.FC<UserListProps> = ({ users }) => {
   const [filterUsers, setFilterUsers] = useState<User[]>(users);
-  const softDelete = async (deleteUserId: number) => {
-    if (confirm("本当にこのユーザーを削除しますか？")) {
+  const [openModal, setOpenModal] = useState(false);
+  const [deleteUserId, setdeleteUserId] = useState<number | null>(null);
+
+  // 削除ボタンを押したときに起動し、選択したユーザーのuserIdをdeleteUserIdにいれる。openModalをtureに。
+  const handleDeleteClick = (userId: number) => {
+    setdeleteUserId(userId);
+    setOpenModal(true);
+  };
+  // モーダルが閉じられたときに起動し、openModalをfalse。deleteUserIdをnull。
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setdeleteUserId(null);
+  };
+  // モーダルでユーザーを削除する選択が選ばれたときに論理削除し、フィルター処理したユーザー一覧を出す。最後にhandleCloseModalを起動し閉じる。
+  const handleConfirmDelete = async () => {
+    if (deleteUserId !== null) {
       try {
         await softDeleteUser(deleteUserId);
         setFilterUsers((filterUsers) =>
           filterUsers.filter((user: User) => user.id !== deleteUserId)
         );
-      } catch (error) {}
+      } catch (error) {
+      } finally {
+        handleCloseModal();
+      }
     }
   };
 
@@ -27,7 +45,7 @@ export const UserList: React.FC<UserListProps> = ({ users }) => {
       {filterUsers.map((user) => (
         <CustomCard
           key={user.id}
-          title={user.name}                 
+          title={user.name}
           description={`メール: ${user.email}\n役割: ${user.role}`}
           actions={
             <>
@@ -47,8 +65,9 @@ export const UserList: React.FC<UserListProps> = ({ users }) => {
               >
                 編集
               </Button>
+              {/*  削除ボタンがクリックされたとき、削除対象のユーザーIDを渡す。モーダルを表示 */}
               <CustomButton
-                onClick={() => softDelete(user.id)}
+                onClick={() => handleDeleteClick(user.id)}
                 variantType="danger"
               >
                 削除
@@ -57,6 +76,14 @@ export const UserList: React.FC<UserListProps> = ({ users }) => {
           }
         />
       ))}
+      {/* モーダルを表示する。削除の確認・キャンセルの動きをここで管理する */}
+      <CustomModal
+        open={openModal}
+        title="ユーザー削除"
+        content="本当にこのユーザーを削除しますか？"
+        onClose={handleCloseModal}
+        onConfirm={handleConfirmDelete}
+      />
     </>
   );
 };
